1d877a201046fa5dbcf583707806e01f906a3d83
ishan@projecttech4dev.org <ishan@projecttech4dev.org>
2026-02-20 06:26:45 +0000

    Cleanup

--- models/compute_achieved.sql
+++
@@ -1,9 +0,0 @@
---DBT AUTOMATION has generated this model, please DO NOT EDIT 
---Please make sure you dont change the model name 
-
-{{ config(materialized='table', schema='intermediate') }}
-WITH cte1 as (
-SELECT *, (achieved_till_25th_nov * 0.75) as compute_achieved_till_25th_nov  FROM {{ref('clean_data_types')}})
--- Final SELECT statement combining the outputs of all CTEs
-SELECT *
-FROM cte1
